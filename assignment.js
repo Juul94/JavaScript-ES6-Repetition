fca2097284877bcdc63898770be6fcdcffb2ee1d
Alexander Sørensen <juul.94@hotmail.com>
2022-02-03 16:54:53 +0100

    JavaScript (ES6) Repetition
    JavaScript (ES6) Repetition

---
+++ assignment.js
@@ -0,0 +1,41 @@
+console.log('Running assignment...');
+
+const transactions = require('./simple-transaction-data').data;
+
+// Assignment 1:
+const filterFunction = (data) => {
+    return data.filter((transaction) => {
+
+        //   1. 'credit_card_company' is 'VISA'
+        //   2. 'transaction_date' is 2015 or later
+        //   3. price is 50 or above
+
+        const newDate = parseInt(transaction.transaction_date.substring(0,4));
+        const newPrice = parseInt(transaction.price);
+
+        if (transaction.credit_card_company == 'VISA') {
+            if(newDate >= 2015) {
+                if(newPrice >= 50) {
+                    return transaction;
+                }
+            }
+        }
+    });
+}
+
+// console.log(filterFunction(transactions));
+
+// Assignment 2:
+const aboveAverageFunction = (data) => {
+
+    const sum = data.map(item => parseInt(item.price)).reduce((a, b) => a + b, 0);
+    const avg = sum / data.length;
+
+    for (let i = 0; i < data.length; i++) {
+        data[i].isPriceAboveAverage = data[i].price >= avg;
+    }
+
+    return data;
+}
+
+console.log(aboveAverageFunction(transactions));
